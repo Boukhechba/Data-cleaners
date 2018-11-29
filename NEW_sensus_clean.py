21d9520a1ff555d556dbef4240dfebe75c75ebc5
Mehdi Boukhechba <mob3f@sys.virginia.edu>
2018-11-29 14:33:47 -0500

    Improve excution time and add further cleaning to script data

--- NEW_sensus_clean.py
+++ NEW_sensus_clean.py
@@ -10,83 +10,94 @@ import os
 import time
 import csv
 import json 
-import shutil
+import pandas as pd
 import gzip
 from collections import defaultdict
 
 
-data_folder = 'D:/swear-test/10_19/raw' # Define data filepath
-target_folder = 'D:/swear-test/10_19/clean' # Define folder to store clean files
 
-# Cuz I'm lazy
-#shutil.rmtree(target_folder)
-#os.makedirs(target_folder)
+data_folder = 'D:/WASH/v1/Uncontrolled' # Define data filepath
+target_folder = 'D:/WASH/v1' # Define folder to store clean files
+
+
 
 absolute_start_time = time.time()
 
 complete_data = defaultdict(list)
 
-filenames = [os.path.join(dp, f) for dp, dn, filenames in os.walk(data_folder) for f in filenames if os.path.splitext(f)[1] == '.gz']
+filenames = [os.path.join(dp, f) for dp, dn, filenames in os.walk(data_folder) for f in filenames if f.endswith('.gz')]
 
 
 for i in range(len(filenames)):
     
     file = filenames[i]
 
-    #print("File " + str(i+1) + " out of " + str(len(filenames)) + ' (' + data_folder + ')') # progress print statement
+    print("File " + str(i+1) + " out of " + str(len(filenames)) + ' (' + data_folder + ')'+ ' (' + file + ')') # progress print statement
+    try:
+        start_time = time.time()
+        if (file.endswith(".gz")):
+            f = gzip.open(file, 'rb')
+        elif(file.endswith(".json")):
+            f = open(file)
+        file_content = f.read()
+        raw_data = json.loads(file_content)
     
-    start_time = time.time()
-    
-    f = gzip.open(file, 'rb')
-    file_content = f.read()
-    raw_data = json.loads(file_content)
-
-    base = ''
-    if 'Swear' in file:
-        base = 'Smartwatch_'
-
-    for line in raw_data:
-        
-        line["Sensus OS"] = line["$type"].split(',')[1]
-        line["Data Type"] = line.pop("$type").split(',')[0]
-        
-        if "PID" in line:
-            line.pop("PID")
-            
-        data_type_split = line["Data Type"].split('.')
-        data_type = data_type_split[len(data_type_split)-1]
-        if data_type[-5:] == "Datum":
-            datum = data_type[:-5]
+        base = ''
+        if 'Swear' in file:
+            base = 'Smartwatch_'
         else:
-            datum = data_type
+            base = 'Sensus_'
+    
+        for line in raw_data:
             
-        file = base + datum + '.csv'
+    
+            if ( 'SWear' in line["$type"]):
+                data_type = line.pop("$type").split('.')[-1]
+                datum = data_type
+            else:
+                line["Sensus OS"] = line["$type"].split(',')[1]
+                line["Data Type"] = line.pop("$type").split(',')[0]
+                data_type_split = line["Data Type"].split('.')
+                data_type = data_type_split[len(data_type_split)-1]
+                if data_type[-5:] == "Datum":
+                    datum = data_type[:-5]
+                else:
+                    datum = data_type
+            if "PID" in line:
+                line.pop("PID")
+                
+                
+            file = base + datum + '.csv'
+            
+            if datum == "Activity":
+                line["Activity Mode"] = line.pop("Activity")
+            complete_data[file].append(line)
+            
+        for key in complete_data.keys():
+            if not (os.path.isfile(os.path.join(target_folder,key))):
+                w = open(os.path.join(target_folder,key),'a')
+                writer = csv.DictWriter(w, fieldnames=complete_data[key][0].keys(),lineterminator='\n')
+                writer.writeheader()
+            writer.writerows(complete_data[key])
+        complete_data.clear()
+        w.flush()
+        elapsed_time = time.time() - start_time
+        elapsed_total_time = time.time() - absolute_start_time        
         
-        if datum == "Activity":
-            line["Activity Mode"] = line.pop("Activity")
+        print("--- " +  time.strftime("%H:%M:%S", time.gmtime(elapsed_time)) +  " / " + time.strftime("%H:%M:%S", time.gmtime(elapsed_total_time)) + " --- " %())
+    except:
+        print("File corrupted")
 
-        complete_data[file].append(line)
 
-    elapsed_time = time.time() - start_time
-    elapsed_total_time = time.time() - absolute_start_time        
+
+if os.path.isfile(os.path.join(target_folder,'Sensus_Script.csv')):
+    data = pd.read_csv(os.path.join(target_folder,'Sensus_Script.csv'),encoding = "ISO-8859-1")
+    data = data.set_index(['RunId'])
+    script_types = data.groupby('ScriptName')
+    for name,group in script_types:
+        data_wide = data.pivot( columns='InputId', values='Response')
+        data_long = data.drop(['InputId','Timestamp','Id','GroupId','CompletionRecords','Response'], 1)
+        data_long = data_long.drop_duplicates()
+        result = pd.concat([data_long, data_wide], axis=1, join='inner')
+        result.to_csv(os.path.join(target_folder,'Sensus_Script_'+name+'.csv'), sep=',', encoding='utf-8',index=True)
     
-    print("--- " +  time.strftime("%H:%M:%S", time.gmtime(elapsed_time)) +  " / " + time.strftime("%H:%M:%S", time.gmtime(elapsed_total_time)) + " --- " %())
-
-
-absolute_start_time = time.time()           
-for key in complete_data.keys():
-    
-    print(key + ' (' + str(len(complete_data[key])) + ')') # progress print statement
-    start_time = time.time()
-    with open(os.path.join(target_folder,key),'w') as f:
-        writer = csv.DictWriter(f, fieldnames=complete_data[key][0].keys(),lineterminator='\n')
-        writer.writeheader()
-        writer.writerows(complete_data[key])
-        f.close()
-        
-    elapsed_time = time.time() - start_time
-    elapsed_total_time = time.time() - absolute_start_time        
-    
-    print("--- " +  time.strftime("%H:%M:%S", time.gmtime(elapsed_time)) +  " / " + time.strftime("%H:%M:%S", time.gmtime(elapsed_total_time)) + " --- " %())
-
-print("Total Program Runtime: " + str(time.time() - absolute_start_time))
